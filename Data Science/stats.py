43e8a4e751a6004c105194b996acf41024c30204
Abdul Moiz <moiz20920@gmail.com>
2025-01-04 22:20:39 +0500

    covered measure of central tendency

--- Data Science/stats.py
+++ Data Science/stats.py
@@ -1,20 +1,53 @@
 import numpy as np
+import pandas as pd
 import statistics
 
-data=[]
-for i in range(1,10):
-    data.append(np.random.randint(0,100))
+import matplotlib.pyplot as plt #visuals
+import seaborn as sns #visual
 
-median=np.median(data)
-mean=np.mean(data)
-mode=statistics.mode(data)
-variance=np.var(data)
-standardDeviation=np.std(data)
+# arr=np.array([1,2,3,4,5]);
+# sum=np.sum(arr) # sum
+# mean=sum/len(arr) # mean
 
-print("Data=",data)
-print("Mean=",mean)
-print("Mode=",mode)
-print("Median=",median)
-print("Variance=",variance)
-print("Standard Deviation=",standardDeviation)
+# arr=np.sort(arr) # sort
+# #builtin functions
+# data=[]
+# for i in range(1,10):
+#     data.append(np.random.randint(0,100))
 
+# median=np.median(data)
+# mean=np.mean(data)
+# mode=statistics.mode(data)
+# variance=np.var(data)
+# standardDeviation=np.std(data)
+
+# print("Data=",data)
+# print("Mean=",mean)
+# print("Mode=",mode)
+# print("Median=",median)
+# print("Variance=",variance)
+# print("Standard Deviation=",standardDeviation)
+
+
+# reading from a file
+dataSet = pd.read_csv("Data Science/people.csv")
+print("Average Height: ",dataSet["Height(cm)"].mean())
+print("Average Weight: ",np.mean(dataSet["Weight(kg)"]))
+
+print("Median Height: ",dataSet["Height(cm)"].median())
+print("Median Weight: ",np.median(dataSet["Weight(kg)"]))
+
+print("Mode Height: ",dataSet["Height(cm)"].mode()[0])
+print("Mode Weight: ",statistics.mode(dataSet["Weight(kg)"]))
+
+
+print("Counts: ",dataSet["Height(cm)"].value_counts())
+
+
+print("Null Values", dataSet.isnull().sum())  # check for null values
+
+sns.histplot(x="Height(cm)",data=dataSet,bins=[i for i in range(140,200,5)])
+plt.plot([dataSet["Height(cm)"].mean() for i in range(0,10)],[i for i in range(0,10)],c="red", label="Mean")
+plt.plot([dataSet["Height(cm)"].median() for i in range(0,10)],[i for i in range(0,10)],c="blue", label="Median")
+plt.plot([dataSet["Height(cm)"].mode() for i in range(0,10)],[i for i in range(0,10)],c="purple" , label="Mode")
+plt.show()
